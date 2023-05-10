c6598f0bea2cc766f7743dab7ddf0bb3086e4dbb
Mohit Kumar <mohit.kumar@transporeon.com>
2023-05-10 16:16:52 +0530

    Adding new style

--- src/app/seller-details/seller-details.component.ts
+++ src/app/seller-details/seller-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router'
 import { sellerSignup } from '../models/seller-signup';
+import {} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-seller-details',
